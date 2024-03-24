966d1318380cf109374cceb188d5f19bac9aeaf5
Milton <deymilton85@gmail.com>
2024-03-24 14:34:26 +0600

    1st commit

---
+++ resources/views/role-permission/permission/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layout.MasterApp')
+@include('role-permission.nav-permission')
+@section('content')
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-md-12">
+
+                @if (session('status'))
+                    <div class="alert alert-success">{{ session('status') }}</div>
+                @endif
+
+                <div class="card mt-3">
+                    <div class="card-header">
+                        <h4>Permissions
+                            <a href="{{ url('permissions/create') }}" class="btn btn-primary float-end">Add Permission</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th width="40%">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($permissions as $permission)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $permission->name }}</td>
+                                    <td>
+                                    
+                                        <a href="{{ url('permissions/'.$permission->id.'/edit') }}" class="btn btn-success">Edit</a>
+                                        
+
+                           
+                                        <a href="{{ url('permissions/'.$permission->id.'/delete') }}" class="btn btn-danger mx-2">Delete</a>
+                                      
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
